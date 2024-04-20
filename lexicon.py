ba2d18b59e59afe7f1ad8ff77f8501da714803a4
Ambuj Kumar Chaubey <114640476+ambujchaubey@users.noreply.github.com>
2024-04-20 22:52:21 +0530

    lexicon.py

--- lexicon.py
+++ lexicon.py
@@ -4,21 +4,21 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-# Download NLTK resources
+
 nltk.download('punkt')
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
 
-# Load the CSV file
+
 df = pd.read_csv('DataPreProcessed.csv')
 
-# Drop rows with NaN values in the 'content' column
+
 df = df.dropna(subset=['content'])
 
-# Initialize SentimentIntensityAnalyzer
+
 sid = SentimentIntensityAnalyzer()
 
-# Function to tokenize text and calculate sentiment score
+
 def analyze_sentiment(text):
     tokens = word_tokenize(text)
     stop_words = set(stopwords.words('english'))
@@ -26,10 +26,10 @@ def analyze_sentiment(text):
     scores = sid.polarity_scores(' '.join(filtered_tokens))
     return scores
 
-# Apply sentiment analysis to the 'content' column
+
 df['sentiment_scores'] = df['content'].apply(lambda x: analyze_sentiment(str(x)))
 
-# Print the sentiment scores
+
 print(df['sentiment_scores'])
 df.to_csv('sentiment_scores.csv', index=False)
 
